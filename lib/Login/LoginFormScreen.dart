25959d3b515f4bb03b375d9473ff7ba46c039151
Jeffrey Shek <jeffshek@gmail.com>
2020-10-16 16:15:25 -0500

    sort of progress lol

--- lib/Login/LoginFormScreen.dart
+++ lib/Login/LoginFormScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:betterself_flutter/constants/theme_constants.dart';
+import 'package:betterself_flutter/components/TextComponents.dart';
+import 'package:betterself_flutter/components/AppButton.dart';
 
 // intl is required by formbuilder
 import 'package:intl/intl.dart';
@@ -16,32 +18,76 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('FormBuilder!')),
+      // appBar: AppBar(title: Text('BetterSelf Login')),
       body: FormBuilder(
         key: _fbKey,
-        initialValue: {
-          'date': DateTime.now(),
-          'accept_terms': false,
-        },
+        initialValue: {'username': "", "password": ""},
         child: Column(
           children: <Widget>[
             Padding(
-                padding: const EdgeInsets.only(left: 25, right: 25, top: 14),
-                child: Column(
-                  children: <Widget>[
-                    FormBuilderTextField(
-                      attribute: "username",
-                      decoration: InputDecoration(labelText: "User Name"),
+              padding: const EdgeInsets.only(left: 25, right: 25, top: 14),
+              child: Column(
+                children: <Widget>[
+                  SafeArea(
+                    child: Container(
+                      padding: EdgeInsets.only(left: 8),
+                      alignment: Alignment.centerLeft,
+                      width: MediaQuery.of(context).size.width,
+                      height: 50,
+                      child: IconButton(
+                        icon: Icon(Icons.arrow_back),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
                     ),
-                    FormBuilderDateTimePicker(
-                      attribute: "date",
-                      inputType: InputType.date,
-                      format: DateFormat("yyyy-MM-dd"),
-                      decoration:
-                          InputDecoration(labelText: "Appointment Time"),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 25, right: 25, top: 14),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        headerText("BetterSelf"),
+                        SizedBox(width: 4),
+                        Padding(
+                            padding: const EdgeInsets.only(bottom: 2),
+                            child: subheaderText("Login"))
+                      ],
                     ),
-                  ],
-                ))
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(25),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          FormBuilderTextField(
+                            attribute: "username",
+                            decoration: InputDecoration(labelText: "User Name"),
+                          ),
+                          SizedBox(height: 25),
+                          FormBuilderTextField(
+                            attribute: "password",
+                            decoration: InputDecoration(labelText: "Password"),
+                            obscureText: true,
+                            maxLines: 1,
+                            // maxLength: 16,
+                            validators: [
+                              FormBuilderValidators.required(),
+                              FormBuilderValidators.minLength(8),
+                              FormBuilderValidators.maxLength(16)
+                            ],
+                          ),
+                          SizedBox(height: 18),
+                          AppButton(
+                            textContent: "Login",
+                            onPressed: () {},
+                          ),
+                        ]),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
